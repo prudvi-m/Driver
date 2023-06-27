f224769517f1f2a815238ed0cc44af833ef5f484
prudvi-m <prudhvi.manukonda4@google.com>
2023-06-27 14:06:27 -0500

    initial

--- Migrations/20230627185848_v1.cs
+++ Migrations/20230627185848_v1.cs
@@ -4,7 +4,7 @@
 
 namespace Drivers.Migrations
 {
-    public partial class Initial : Migration
+    public partial class v1 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
